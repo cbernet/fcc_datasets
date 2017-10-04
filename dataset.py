070c6c5bdc42aa686d4f43b0453e6b7424c9dfc4
Colin <colin.bernet@cern.ch>
2017-10-04 11:16:21 +0200

    interface to prepare for yaml creation

--- dataset.py
+++ dataset.py
@@ -16,7 +16,12 @@ class File(object):
         self._check()
 
     def good(self):
+        '''is the file good?'''
         return self.flags['good']
+    
+    def nevents(self):
+        '''return the number of events.'''
+        return self.flags['n_events']
 
     def _check(self):
         self.flags = dict()
@@ -60,6 +65,7 @@ class Dataset(Directory):
         else:
             self.path = basedir.abspath(name)
             self.all_files = dict()
+            self.good_files = dict()
             self._build_list_of_files(pattern)
             self._write_to_cache()
 
@@ -67,10 +73,23 @@ class Dataset(Directory):
         for path in glob.glob(self.abspath(pattern)):
             the_file = File(path)
             self.all_files[the_file.name] = the_file
-            print the_file
+            if the_file.good():
+                self.good_files[the_file.name] = the_file
+        
+    def nfiles(self):
+        '''return the total number of files, good or not'''
+        return len(self.all_files)
+
+    def ngoodfiles(self):
+        """return the number of good files."""
+        return len(self.good_files)
             
     def list_of_good_files(self):
         return [the_file.path for the_file in self.all_files.values() if the_file.good()]
+
+    def nevents(self):
+        '''Returns the sum of the number of events in all good files'''
+        return sum([f.nevents() for f in self.good_files.values()])
 
     def _read_from_cache(self):
         sh = shelve.open(self._cache_fname())
--- test_dataset.py
+++ test_dataset.py
@@ -1,6 +1,10 @@
 import unittest
+import os
+import basedir
 from dataset import Dataset
 
+if not os.path.isdir(basedir.basename):
+    basedir.basename = os.path.abspath('data')
 dataset_name = 'papas/ee_to_ZZ_1oct_A_1'
 dataset_pattern = '*.0_1*.root'
 
@@ -11,6 +15,9 @@ class TestDataset(unittest.TestCase):
         dataset = Dataset(dataset_name, dataset_pattern, cache=False)
         self.assertEqual(len(dataset.all_files),11)
         self.assertEqual(len(dataset.list_of_good_files()),9)
+        self.assertEqual(dataset.nfiles(), 11)
+        self.assertEqual(dataset.ngoodfiles(), 9)
+
 
     def test_2_cache(self):
         '''Test dataset reading from cache'''
@@ -18,22 +25,29 @@ class TestDataset(unittest.TestCase):
         self.assertEqual(len(dataset.all_files),11)
         self.assertEqual(len(dataset.list_of_good_files()),9)
 
-    def test_3_print(self):
+    #----------------------------------------------------------------------
+    def test_3_nevents(self):
+        """Test that the number of events is correct"""
         dataset = Dataset(dataset_name, dataset_pattern, cache=True)
-        printout = '''papas/ee_to_ZZ_1oct_A_1
-/eos/experiment/fcc/ee/datasets/papas/ee_to_ZZ_1oct_A_1
-papasout_2.0_15.root           : {'good': True, 'n_events': 100000L, 'zero_size': False}
-papasout_2.0_12.root           : {'good': True, 'n_events': 100000L, 'zero_size': False}
-papasout_2.0_18.root           : {'good': False, 'n_events': None, 'zero_size': True}
-papasout_2.0_17.root           : {'good': True, 'n_events': 100000L, 'zero_size': False}
-papasout_2.0_1.root            : {'good': True, 'n_events': 100000L, 'zero_size': False}
-papasout_2.0_11.root           : {'good': True, 'n_events': 100000L, 'zero_size': False}
-papasout_2.0_14.root           : {'good': True, 'n_events': 100000L, 'zero_size': False}
-papasout_2.0_13.root           : {'good': False, 'n_events': None, 'zero_size': True}
-papasout_2.0_19.root           : {'good': True, 'n_events': 100000L, 'zero_size': False}
-papasout_2.0_16.root           : {'good': True, 'n_events': 100000L, 'zero_size': False}
-papasout_2.0_10.root           : {'good': True, 'n_events': 100000L, 'zero_size': False}'''
-        self.assertMultiLineEqual(str(dataset),printout)
+        self.assertEqual(dataset.nevents(), 900000)
+        
+
+##    def test_3_print(self):
+##        dataset = Dataset(dataset_name, dataset_pattern, cache=True)
+##        printout = '''papas/ee_to_ZZ_1oct_A_1
+##/eos/experiment/fcc/ee/datasets/papas/ee_to_ZZ_1oct_A_1
+##papasout_2.0_15.root           : {'good': True, 'n_events': 100000L, 'zero_size': False}
+##papasout_2.0_12.root           : {'good': True, 'n_events': 100000L, 'zero_size': False}
+##papasout_2.0_18.root           : {'good': False, 'n_events': None, 'zero_size': True}
+##papasout_2.0_17.root           : {'good': True, 'n_events': 100000L, 'zero_size': False}
+##papasout_2.0_1.root            : {'good': True, 'n_events': 100000L, 'zero_size': False}
+##papasout_2.0_11.root           : {'good': True, 'n_events': 100000L, 'zero_size': False}
+##papasout_2.0_14.root           : {'good': True, 'n_events': 100000L, 'zero_size': False}
+##papasout_2.0_13.root           : {'good': False, 'n_events': None, 'zero_size': True}
+##papasout_2.0_19.root           : {'good': True, 'n_events': 100000L, 'zero_size': False}
+##papasout_2.0_16.root           : {'good': True, 'n_events': 100000L, 'zero_size': False}
+##papasout_2.0_10.root           : {'good': True, 'n_events': 100000L, 'zero_size': False}'''
+##        self.assertMultiLineEqual(str(dataset),printout)
 
 
 

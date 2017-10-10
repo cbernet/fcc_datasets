560f90b735694d82acfb6dceca43b6871d17c564
Colin <colin.bernet@cern.ch>
2017-10-10 10:50:49 +0200

    unittest for publication script

---
+++ scripts/test_publish.py
@@ -0,0 +1,22 @@
+import unittest
+import os
+import random
+from dataset import Dataset
+
+class TestPublish(unittest.TestCase):
+    
+    #----------------------------------------------------------------------
+    def test_1_publish(self):
+        """Test that the publish script is working."""
+        xsection = random.uniform(0, 1)
+        dsname = 'heppy/ee_to_ZZ_condor_A_703' 
+        cmd = 'python publish.py {} -x {}'.format(
+            dsname, 
+            xsection
+        )
+        os.system(cmd)        
+        dset = Dataset(dsname)
+        self.assertAlmostEqual(dset.xsection(), xsection, places=7)
+
+if __name__ == '__main__':
+    unittest.main()

db2cf18945a9eaf23ae153968138f25fa09bd9ba
Colin <colin.bernet@cern.ch>
2017-10-27 15:40:35 +0200

    simplified fccdataset interface

--- fcc_component.py
+++ fcc_component.py
@@ -4,13 +4,11 @@ from fcc_datasets.dataset import Dataset
 class FCCComponent(cfg.MCComponent):
     
     #----------------------------------------------------------------------
-    def __init__(self, name, cache=True,
-                 xsection=None, **kwargs):
+    def __init__(self, name, cache=True, **kwargs):
         """"""
         dataset = Dataset(name, extract_info=False, cache=cache)
         super(FCCComponent, self).__init__(
             dataset.name,
             dataset.list_of_good_files(),
-            xSection=xsection, 
             **kwargs
         )
